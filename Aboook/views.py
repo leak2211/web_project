c6eb5a90b30b47b2fbeda32826bfd80e3b4f510e
leak2211 <144492917+leak2211@users.noreply.github.com>
2026-04-23 12:05:03 +0300

    add templates

--- Aboook/views.py
+++ Aboook/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'Aboook/index.html')
+    context = {
+        'title': 'Главная страница',
+        'welcome_text': 'Aboook - обменивайтесь книгами',
+    }
+    return render(request, 'Aboook/index.html', context)
+
+def about(request):
+    context = {
+        'title': 'О нас',
+    }
+    return render(request, 'Aboook/about.html', context)
--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Aboook.views import index   
+from Aboook.views import index, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='home'),   
+    path('', index, name='home'),
+    path('about/', about, name='about'),
 ]
