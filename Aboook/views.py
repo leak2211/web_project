c98b9e4006e93f10af35ade64b22e6ecad2993f4
leak2211 <144492917+leak2211@users.noreply.github.com>
2026-04-30 13:06:32 +0300

    Task 6 ready.

--- Aboook/views.py
+++ Aboook/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from .models import Book  
 
 def index(request):
+    books = Book.objects.all()
+    
     context = {
         'title': 'Главная страница',
         'welcome_text': 'Aboook - обменивайтесь книгами',
+        'books': books,   
     }
     return render(request, 'Aboook/index.html', context)
 
