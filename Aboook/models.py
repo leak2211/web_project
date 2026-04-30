8cab557608079a8b874af7029346d64cf2201c66
leak2211 <144492917+leak2211@users.noreply.github.com>
2026-04-30 12:36:58 +0300

    Task 5 ready.

--- Aboook/models.py
+++ Aboook/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    year = models.IntegerField(verbose_name='Год издания')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+    author = models.CharField(max_length=200, blank=True, verbose_name='Автор')
+
+    def __str__(self):
+        return f"{self.title} ({self.author})"
+
+    class Meta:
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
+        ordering = ['-created_at']
