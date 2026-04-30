0c376de553e79c8bbe376b1897ff4901ad5e6517
leak2211 <144492917+leak2211@users.noreply.github.com>
2026-04-30 14:30:38 +0300

    Task 7

--- Aboook/views.py
+++ Aboook/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
-from .models import Book  
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
 def index(request):
     books = Book.objects.all()
-    
     context = {
         'title': 'Главная страница',
         'welcome_text': 'Aboook - обменивайтесь книгами',
-        'books': books,   
+        'books': books,
     }
     return render(request, 'Aboook/index.html', context)
 
@@ -16,3 +15,11 @@ def about(request):
         'title': 'О нас',
     }
     return render(request, 'Aboook/about.html', context)
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    context = {
+        'title': book.title,
+        'book': book,
+    }
+    return render(request, 'Aboook/detail.html', context)
--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Aboook.views import index, about
+from Aboook.views import index, about, book_detail   
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home'),
     path('about/', about, name='about'),
+    path('book/<int:pk>/', book_detail, name='book_detail'),   
 ]
